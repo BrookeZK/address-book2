cdefd32af9899c73f24ee2590ad50f2f592f828e
AJ Ancheta <ashleyjancheta@gmail.com>
2019-04-08 07:42:24 -0700

    add find and delete contact function

--- js/scripts.js
+++ js/scripts.js
@@ -25,6 +25,28 @@ AddressBook.prototype.addContact = function(contact) {
   this.contacts.push(contact);
 }
 
+AddressBook.prototype.findContact = function (id) {
+  for (var i=0; i < this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        return this.contacts[i];
+    }
+   }
+  };
+  return false;
+}
+
+AddressBook.prototype.deleteContact = function (id) {
+  for (var i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id ==id) {
+        delete this.contacts[i];
+        return true;
+    }
+   }
+  };
+  return false;
+}
 // Business Logic for Contacts
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
